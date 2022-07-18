f654ef8c2686bd65bf62a2b75936dd342244a375
MarisaCodes <marisakirisame955@gmail.com>
2022-07-19 03:46:08 +0400

    refactored code, improved styling and made JS file less bloated

--- app.js
+++ app.js
@@ -1,104 +1,71 @@
-// const deleteBtn = document.querySelectorAll('.fa-trash-can');
+// li tag to be appended
 
-// deleteBtn.forEach(element => {
-//     element.addEventListener('click',function(event){
-//         event.target.parentElement.remove();
-//     })
-// });
+let userLiInnerHtml = `
+<div>
+  <input type="checkbox" class="check-box" />
+  <span class="todo"></span>
+</div>
+<div>
+  <i class="fa-solid fa-trash-can"></i>
+  <i class="fa-solid fa-pen-to-square"></i>
+</div>`;
+const test = document.querySelector("input");
+console.log(test.getAttribute("type"));
+let editFormInnerHtml = `
+        
+            <input type="text">
+            <button type="submit">Edit</button>
+        `;
 
-//above code did not work so I fixed it below
+const form = document.getElementById("add-todo");
+const ul = document.getElementById("todo-ul");
 
-
-const form = document.querySelector('form');
-const ul = document.querySelector('ul');
-const btn = document.querySelector('button');
-
-ul.addEventListener('click',function(event){
-    if(event.target.classList.contains('fa-trash-can')){
-        event.target.parentElement.remove();
-    }
-})
-
-
-
-ul.addEventListener('click',function(event){
-    if(event.target.classList.contains('fa-pen-to-square')){
-        if(!event.target.parentElement.querySelector('#exists')) {
-        let editForm = document.createElement('form');
-        editForm.className = 'edit';
-        editForm.setAttribute('id','exists');
-        let editTodo = document.createElement('input');
-        let addEdit = document.createElement('button');
-        editTodo.className = 'edit-input';
-        editTodo.setAttribute('style','padding:6px;margin:5px;');
-        addEdit.setAttribute('style','padding:5px;color:blue;cursor:pointer;');
-        addEdit.innerText = 'Edit';
-        addEdit.className = 'edit';
-        editTodo.setAttribute('type','text');
-        addEdit.setAttribute('type','submit');
-        editForm.appendChild(editTodo);
-        editForm.appendChild(addEdit);
-        let span = event.target.parentElement.querySelector('.todo');
-        editTodo.value = span.innerText;
-        event.target.parentElement.querySelector('.todo').innerText = '';
-        let li = event.target.parentElement;
-        li.appendChild(editForm);
-    }
-    }
-})
-
-
-
-
-ul.addEventListener('submit',function(event){
-    if(event.target.classList.contains('edit'))
-    event.preventDefault();
-    let li = event.target.parentElement;
-    let span = event.target.parentElement.querySelector('.todo');
-    if(event.target.parentElement.querySelector('.edit-input').value == '') {
-        event.target.parentElement.remove();
-    }
-    let editText = event.target.parentElement.querySelector('.edit-input').value;
-    span.innerText = editText;
-    event.target.remove();
-})
-
-
-
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-    let li = document.createElement('li');
-    let span = document.createElement('span');
-    let checkBox = document.createElement('input');
-    checkBox.className = 'check-box';
-    checkBox.setAttribute('type', 'checkbox');
-    li.appendChild(checkBox);
-    span.className = 'todo';
-    let todoText = document.querySelector('#todo-text').value;
-    span.innerText = todoText;
-    li.appendChild(span);
-    let trashCan = document.createElement('i')
-    trashCan.classList = 'fa-solid fa-trash-can';
-    li.appendChild(trashCan);
-    let edit = document.createElement('i');
-    edit.classList = "fa-solid fa-pen-to-square";
-    li.appendChild(edit);
-    ul.appendChild(li);
-    if(document.querySelector('#todo-text').value == '') {
-        li.remove();
-    }
-    document.querySelector('#todo-text').value ='';
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let input = form.firstElementChild.value;
+  if (input != "") {
+    let userLi = document.createElement("li");
+    userLi.innerHTML = userLiInnerHtml;
+    userLi.querySelector(".todo").innerText = input;
+    ul.appendChild(userLi);
+    form.firstElementChild.value = null;
+  }
 });
 
-ul.addEventListener('click',function(event){
-    
-    if(event.target.nextElementSibling.classList.contains('completed')) {
-        event.target.nextElementSibling.classList.remove('completed');
-        }
-
-    else if(event.target.classList.contains('check-box')){
-        if(!event.target.nextElementSibling.classList.contains('completed')) {
-        event.target.nextElementSibling.classList.add('completed');
-        }
+ul.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-trash-can")) {
+    e.target.parentElement.parentElement.remove();
+  }
+  if (e.target.classList.contains("fa-pen-to-square")) {
+    let editText =
+      e.target.parentElement.parentElement.querySelector("span").innerText;
+    e.target.parentElement.parentElement.querySelector("span").remove();
+    let editForm = document.createElement("form");
+    editForm.className = "edit-form";
+    editForm.innerHTML = editFormInnerHtml;
+    editForm.querySelector("input").value = editText;
+    e.target.parentElement.parentElement.firstElementChild.appendChild(
+      editForm
+    );
+  }
+  if (e.target.getAttribute("type") == "checkbox") {
+    if (e.target.nextElementSibling.tagName == "SPAN") {
+      if (e.target.checked) {
+        e.target.nextElementSibling.classList.add("completed");
+      } else {
+        e.target.nextElementSibling.classList.remove("completed");
+      }
     }
-})
+  }
+});
+
+ul.addEventListener("submit", (e) => {
+  if (e.target.classList.contains("edit-form")) {
+    e.preventDefault();
+    let newText = e.target.firstElementChild.value;
+    let newSpan = document.createElement("span");
+    newSpan.innerText = newText;
+    e.target.parentElement.appendChild(newSpan);
+    e.target.remove();
+  }
+});
